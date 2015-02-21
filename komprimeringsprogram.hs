32f73ec9da8769b17b8a87f128154bed8d3a945c
André Le Blanc <leblanc_112@hotmail.com>
2015-02-22 00:39:24 +0100

    testar

---
+++ komprimeringsprogram.hs
@@ -0,0 +1,42 @@
+{-foor binary strings-}
+import Data.Binary
+import qualified Data.ByteString as B
+
+data word = (String, Int)
+data library = [word]
+
+{- 	compress String
+	PRE: TRUE
+	POST: The string compressed and turned into a bytestring.
+-}
+compress :: String -> B.ByteString 
+compress text = undefined
+
+
+{- 	Buildlibrary
+	PRE: TRUE
+	POST: an extention of the standard library for the specific file
+-}
+buildlibrary :: B.ByteString -> String -> B.ByteString
+buildlibrary = undefined
+
+{- decomress
+	PRE: TRUE
+	POST: The file decyptet from a binary file to a String
+-}
+decompress :: B.ByteString -> String
+decompress = undefined
+
+{-	encrypt
+	PRE: an encypted file
+	POST: The file encrypted with the RSA algorithm
+-}
+encrypt :: B.ByteString -> String -> B.ByteString
+encrypt = undefined
+
+{- decrypt
+	PRE: True
+	Post: If the password is correct the binary file decrypted. Otherwise the same binary file.
+-}
+decrypt :: B.ByteString -> B.ByteString
+decrypt = undefined
