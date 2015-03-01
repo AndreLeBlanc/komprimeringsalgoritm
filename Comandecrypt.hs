bc46700b059d3445a731872030e8bfdb51eedc4b
André Le Blanc <leblanc_112@hotmail.com>
2015-03-01 02:08:53 +0100

    hej

--- Comandecrypt.hs
+++ Comandecrypt.hs
@@ -1,13 +1,10 @@
 module Comandecrypt where
-
-lzcomp :: [Int] -> [Int]
-lzcomp squashMe = lzcomp1 (drop 4 squashMe) 0 (take 4 squashMe)
-
-lzcomp1 :: [Int] -> Int -> [Int] -> [Int]
-lzcomp1 squashMe pos acc
-	| pos >= length squashMe - 1 = acc 	
-    | snd compressable > 3 = lzcomp1 squashMe (pos +1) ([127, fst compressable, snd compressable]++ acc)
-	| otherwise = lzcomp1 squashMe (pos + 1) ([squashMe !! pos] ++ acc)
+import Data.List
+lzcomp :: Int -> [Int] -> [Int]
+lzcomp pos squashMe
+	| pos >= length squashMe = [] 	
+    | snd compressable > 3 = [127, fst compressable, snd compressable] ++ (lzcomp (pos + (snd compressable)) squashMe)
+	| otherwise =  (squashMe !! pos):(lzcomp (pos + 1) squashMe)
 	where
 		compressable :: (Int, Int)
 		compressable = search (largest $ reverse compressable') compressable'
@@ -20,30 +17,32 @@ lzcomp1 squashMe pos acc
 		largest lista = maximum $ map snd lista
 
 		compressable' :: [(Int, Int)]
-		compressable' 	| pos > 132 = map (numberofjumps pos 0) [(pos-132)..(pos-4)]
-						| otherwise = map (numberofjumps pos 0) [0..(pos-4)]
+		compressable' 	
+			| pos > 127 = map (numberofjumps pos 0) [(pos-126)..(pos-4)]
+			| otherwise = map (numberofjumps pos 0) [0..(pos-4)]
 
 		numberofjumps :: Int -> Int -> Int -> (Int, Int)
 		numberofjumps pos jumps checkPos	
-											| (checkPos + jumps + 1 == pos) = (checkPos, jumps)
-											| (checkPos +  jumps) < (pos) && squashMe !! (checkPos + jumps) == squashMe !! (pos +  jumps) = (checkPos, (1 + (snd (numberofjumps pos (jumps+1) checkPos))))
-											| otherwise = (0, 0)
+			| checkPos + jumps >= pos || length squashMe <= (pos + jumps) = (checkPos, jumps)
+			| squashMe !! (checkPos + jumps) == squashMe !! (pos +  jumps) = (checkPos, (snd (numberofjumps pos (jumps+1) checkPos)))
+			| otherwise = (0, 0)
 
-ldecomp a= [1,2,3,4]
-lzdecomp :: [Int] -> [Int]
-lzdecomp unSquash = lzdecomp1 unSquash 0 []
-
-lzdecomp1 :: [Int] -> Int -> [Int] -> [Int]
-lzdecomp1 unSquash pos acc
-	| pos >= length unSquash = acc	
-	| unSquash !! pos /= 127 = lzdecomp1 unSquash (pos + 1) (acc ++ [unSquash !! pos])
-	| otherwise = lzdecomp1 unSquash (pos + 3) (acc ++ (dezip 0))
+lzdecomp :: Int -> [Int] -> [Int]
+lzdecomp pos unSquash 
+	| pos >= length unSquash = unSquash	
+	| genericIndex unSquash pos /= 127 =  lzdecomp (pos + 1) unSquash
+	| otherwise = lzdecomp (pos + (length (dezip 0)) +1) ((take pos unSquash) ++ (dezip 0) ++ (drop (pos + 3) unSquash))
 	where
+		steps :: Int
 		steps = unSquash !! (pos + 2)
+
+		getPos :: Int
 		getPos = pos - (unSquash !! (pos + 1))
+
 		dezip :: Int -> [Int]
 		dezip jump	| steps > jump + 1 =  [unSquash !! (getPos + jump)] ++ (dezip (jump + 1))
 					| otherwise = [unSquash !! (getPos + jump)]
+
 hash :: [Int] -> [Int]
 hash = undefined
 
